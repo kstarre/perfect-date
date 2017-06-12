929a811922a2f5f3e157e00e0913d770fa1278d4
Nick Malakar <nickmalakar@gmail.com>
2017-06-12 10:03:49 -0400

    First update GSAP

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -1,15 +1,18 @@
-// For highlight and enlarge image when hovering
+// For highlight, add text and enlarge image when hovering
 
 // When clicking images
 // Click on the left image - both images move off page
-$("#leftImage").click(function(){
-	TweenMax.to($("#leftImage"), 1, {x:"-600px", ease:Linear.easeNone, force3D:true});
-	TweenMax.to($("#rightImage"), 1, {x:"1200px", ease:Linear.easeNone, force3D:true});
+var leftImage = $("#leftImage");
+var rightImage = $("#rightImage");
+
+leftImage.click(function(){
+	TweenMax.to(leftImage, 1, {x:"-1200px", ease:Linear.easeNone, force3D:true});
+	TweenMax.to(rightImage, 1, {x:"1200px", ease:Linear.easeNone, force3D:true});
 });
 
 // Click on right image - both images move off page
-$("#rightImage").click(function(){
-	TweenMax.to($("#leftImage"), 1, {x:"-600px", ease:Linear.easeNone, force3D:true});
-	TweenMax.to($("#rightImage"), 1, {x:"1200px", ease:Linear.easeNone, force3D:true});
+rightImage.click(function(){
+	TweenMax.to(leftImage, 1, {x:"-1200px", ease:Linear.easeNone, force3D:true});
+	TweenMax.to(rightImage, 1, {x:"1200px", ease:Linear.easeNone, force3D:true});
 });
 

9d9455745f14ac1c1b36e38d2201cc72207ac14a
Katie Starrett <starrett.k.e@gmail.com>
2017-06-13 15:50:42 -0400

    added ajax request for movies

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,3 +15,32 @@ var database = firebase.database
 	// go to yelp and pull restaurants
 //when click on events, go to eventbrite and pull events
 	// go to yelp and pull restaurants
+
+// ajax
+$(window).on("load", function() {
+	var queryURL = "http://data.tmsapi.com/v1.1/movies/showings" + "?startDate=2017-06-13&zip=44131" + "&api_key=6n4cata848e7z3fha7nkgb77";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+		console.log(response[0].title);
+
+		for (var i = 0; i < response.length; i++) {
+			var subsection = $("<div>");
+			var title = $("<p>");
+			var times = $("<p>");
+			title.html(response[i].title);
+			for (var j = 0; j < response[i].showtimes.length; j++) {
+				times.append(response[i].showtimes[j].dateTime + ", ");
+			}
+			title.append(times);
+			subsection.append(title);
+			$("#movieEventHolder").append(subsection);
+
+		};
+	})
+
+});
+

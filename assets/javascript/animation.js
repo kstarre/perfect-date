127cb53beff8aabfd449a9136109d3dac00d5cfc
Katie Starrett <starrett.k.e@gmail.com>
2017-06-16 11:40:47 -0400

    updating my files with JS and NM html, css, and animation

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -4,11 +4,15 @@ var logo = $("#headerLogo");
 
 var leftImage = $("#leftImage");
 var rightImage = $("#rightImage");
-var movie = "Choose Movies";
-var event = "Choose Events";
+var movie = $("#movieText");
+var event = $("#eventText");
+var button = $("#perfectButton");
+var buttonText = $(".textAbovePerfectButton");
+
+var movieChoice = $(".movieChoice");
 
 // On load for site
-TweenMax.from(logo, 2, {ease: Back.easeOut.config(2), x: -1000});
+TweenMax.from(logo, 1.25, {ease: Back.easeOut.config(2), x: -1000});
 
 
 // Image enlarge on hover
@@ -17,6 +21,7 @@ leftImage.hover(function() {
 	TweenMax.to($(this), 0.3, {scale: 1.1});
 	// Box shadow
 	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255, 51,0.7)", repeat: -1, yoyo: true, ease: Linear.easeNone});
+	TweenMax.fromTo(movie, 1, {autoAlpha: 0}, {x:-450, y:-800, scale: 1.2});
 },
 function() {// Back to normal
 	TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
@@ -36,13 +41,28 @@ function() {// back to normal
 leftImage.click(function(){
 	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
 	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -650, y: -200, scale:0.35});// Moves logo
+	TweenMax.to(logo, 1.5, {x: -725, y: -290, scale:0.25});// Moves logo
+	TweenMax.to([perfectButton, buttonText], 1, {autoAlpha: 0});// Removes button and text
 });
 
 // Click on right image - both images move off page
 rightImage.click(function(){
 	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
 	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -650, y: -200, scale:0.35});// Moves logo
+	TweenMax.to(logo, 1.5, {x: -725, y: -290, scale:0.25});// Moves logo
+	TweenMax.to([perfectButton, buttonText], 1, {autoAlpha: 0});// Removes button and text
 });
 
+// Movie choice animation
+movieChoice.click(function() {
+	TweenMax.to($(this), 2, {scale: 1.5});
+});
+
+/*
+{textShadow:"2px 2px 15px white", color: "red"}
+movieChoice.click(function() {
+	TweenMax.from(restaurantDiv, 2, {//slide from offpage to onpage});
+});
+*/
+
+//console.log(movieChoice);
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,44 +9,88 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database
-
+var database = firebase.database();
+// my gracenote 6n4cata848e7z3fha7nkgb77
+// nick's gracenote API zephc9snecc3dpg2eh66m4ng
+ 
+var movieObject = []; 
 // Gracenote API and OMDB API
-$(window).on("load", function() {
-	var queryURL = "http://data.tmsapi.com/v1.1/movies/showings" + "?startDate=2017-06-13&zip=44131" + "&api_key=6n4cata848e7z3fha7nkgb77";
-
+$("#movie-click").on("click", function() {
+	var date = moment().format("YYYY-MM-DD");
+	var location = 44131;
+	var gracenoteQueryURL = "http://data.tmsapi.com/v1.1/movies/showings" + "?startDate=" + date + "&zip=" + location + "&api_key=zephc9snecc3dpg2eh66m4ng";
 	$.ajax({
-		url: queryURL,
+		url: gracenoteQueryURL,
 		method: "GET"
-	}).done(function(response) {
-		console.log(response);
+	}).done(function(gracenoteResponse) {
+		movieObject = gracenoteResponse;
+		console.log(movieObject);
 
-		for (var i = 0; i < response.length; i++) {
+		for (var i = 0; i < gracenoteResponse.length; i++) {
+			var subsection = $("<div>");
+			var title = $("<p>");
+			subsection.addClass("movieChoice");
+			subsection.attr("data-name", gracenoteResponse[i].title);
+			title.html(gracenoteResponse[i].title);
+			subsection.append(title);
+			$("#movieEventHolder").append(subsection);
+		};
+	});
+});
+
+$(document).on("click", ".movieChoice", function() {
+	for(var i = 0; i < movieObject.length; i++) {	
+		if( $(this).attr("data-name") === movieObject[i].title) {
+
 			var subsection = $("<div>");
 			var title = $("<p>");
 			var times = $("<p>");
-			title.html(response[i].title);
-			for (var j = 0; j < response[i].showtimes.length; j++) {
-				times.append(response[i].showtimes[j].dateTime + ", ");
-			}
-			title.append(times);
+			var goBack = $("<button>");
+			goBack.html("Want a different movie?");
+			goBack.attr("id", "goBackButton")
+			title.html(movieObject[i].title);
 			subsection.append(title);
-			$("#movieEventHolder").append(subsection);
-
-		};
-	});
-
+			for (var j = 0; j < movieObject[i].showtimes.length; j++) {
+				times.append(movieObject[i].showtimes[j].dateTime + " ");
+			};
+			subsection.append(times);
+			subsection.append(goBack);
+			$("#movieEventHolder").html(subsection);
+			database.ref().push( {
+				movie: movieObject[i].title
+			});
+		}
+	};
 });
 
+$(document).on("click", "#goBackButton", function(event) {
+	event.preventDefault();
+	for(var i = 0; i < movieObject.length; i++) {
+		var subsection = $("<div>");
+		var title = $("<p>");
+		subsection.addClass("movieChoice");
+		subsection.attr("data-name", movieObject[i].title);
+		title.html(movieObject[i].title);
+		subsection.append(title);
+		$("#movieEventHolder").html(subsection);
+	};
+});
+
+
+
+
+
+
 // Event Brite API
-var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=WJ5ZSOV6TV56IC44E7EJ&location.address=Cleveland";
+/*$("#event-click").on("click", function() { 
+	var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=WJ5ZSOV6TV56IC44E7EJ&location.address=Cleveland";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
+	  $.ajax({
+	    url: queryURL,
+	    method: "GET"
 
-  }).done(function (event) {
-    //full object
-    console.log(event);
-
-  });
+	  }).done(function (event) {
+	    //full object
+	    console.log(event);
+	  });
+});*/

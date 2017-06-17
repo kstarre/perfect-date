edd4732565c0a4b5cac265a317fa094305435baf
Nick Malakar <nickmalakar@gmail.com>
2017-06-17 10:43:00 -0400

    updated animation

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -4,8 +4,7 @@ var logo = $("#headerLogo");
 
 var leftImage = $("#leftImage");
 var rightImage = $("#rightImage");
-var movie = $("#movieText");
-var event = $("#eventText");
+var button = $("button");
 var button = $("#perfectButton");
 var buttonText = $(".textAbovePerfectButton");
 
@@ -16,10 +15,6 @@ var restaurant = $(".restaurant");
 // On load for site
 TweenMax.from(logo, 1.25, {ease: Back.easeOut.config(2), x: -1000});
 
-restaurant.hover(function(){
-	TweenMax.to(restaurant, 1.5, {rotation: 100});
-});
-
 
 // Image enlarge on hover
 leftImage.hover(function() {
@@ -27,7 +22,16 @@ leftImage.hover(function() {
 	TweenMax.to($(this), 0.3, {scale: 1.1});
 	// Box shadow
 	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255, 51,0.7)", repeat: -1, yoyo: true, ease: Linear.easeNone});
-	TweenMax.fromTo(movie, 1, {autoAlpha: 0}, {x:-450, y:-800, scale: 1.2});
+	},
+	function() {// Back to normal
+		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
+});// End left hover
+// Image enlarge on hover
+restaurant.hover(function() {
+	// Increase scale
+	TweenMax.to($(this), 0.3, {scale: 1.1});
+	// Box shadow
+	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255, 51,0.7)", repeat: -1, yoyo: true, ease: Linear.easeNone});
 	},
 	function() {// Back to normal
 		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
@@ -43,20 +47,31 @@ rightImage.hover(function() {
 		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
 });// End right hover
 
-// Click on left image - both images move off page
-leftImage.click(function(){
-	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
+// Click and move function
+function click() {
+	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true, onComplete: hideOpeningPage});
 	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -725, y: -290, scale:0.25});// Moves logo
+	TweenMax.to(logo, 1.5, {x: -725, y: -290, rotation: 360, scale:0.25});// Moves logo
 	TweenMax.to([perfectButton, buttonText], 1, {autoAlpha: 0});// Removes button and text
-});
+};
+
+//hide opening page div onComplete
+function hideOpeningPage() {
+	$("#openingPage").hide();// hides opening page
+	console.log(hideOpeningPage);
+}
+
+// Click on left image - both images move off page
+leftImage.click(click);
 
 // Click on right image - both images move off page
-rightImage.click(function(){
-	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
-	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -725, y: -290, scale:0.25});// Moves logo
-	TweenMax.to([perfectButton, buttonText], 1, {autoAlpha: 0});// Removes button and text
+rightImage.click(click);
+
+// Click on button animation
+button.click(click); 
+
+$(document).on("mouseenter", ".restaurant", function(){
+	TweenMax.to(restaurant, 1.5, {rotation: 100});
 });
 
 // Restaurant
@@ -82,5 +97,3 @@ movieChoice.click(function() {
 	TweenMax.from(restaurantDiv, 2, {//slide from offpage to onpage});
 });
 */
-
-//console.log(movieChoice);

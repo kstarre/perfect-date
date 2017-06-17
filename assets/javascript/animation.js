40b810ee505756bbe80e8103ce8dcf723d75c221
Nicholas R Sajka <nicksajka@gmail.com>
2017-06-17 12:00:58 -0400

    eventBrite API working in index

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -4,11 +4,16 @@ var logo = $("#headerLogo");
 
 var leftImage = $("#leftImage");
 var rightImage = $("#rightImage");
-var movie = "Choose Movies";
-var event = "Choose Events";
+var button = $("button");
+var button = $("#perfectButton");
+var buttonText = $(".textAbovePerfectButton");
+
+//var movieChoice = $(".movieChoice");
+var restaurant = $(".restaurant");
+
 
 // On load for site
-TweenMax.from(logo, 2, {ease: Back.easeOut.config(2), x: -1000});
+TweenMax.from(logo, 1.25, {ease: Back.easeOut.config(2), x: -1000});
 
 
 // Image enlarge on hover
@@ -17,32 +22,76 @@ leftImage.hover(function() {
 	TweenMax.to($(this), 0.3, {scale: 1.1});
 	// Box shadow
 	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255, 51,0.7)", repeat: -1, yoyo: true, ease: Linear.easeNone});
-},
-function() {// Back to normal
-	TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
-});
+	},
+	function() {// Back to normal
+		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
+});// End left hover
+// Image enlarge on hover
+restaurant.hover(function() {
+	// Increase scale
+	TweenMax.to($(this), 0.3, {scale: 1.1});
+	// Box shadow
+	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255, 51,0.7)", repeat: -1, yoyo: true, ease: Linear.easeNone});
+	},
+	function() {// Back to normal
+		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
+});// End left hover
 
 rightImage.hover(function() {
 	TweenMax.to($(this), 0.3, {scale: 1.1});
 	// Box shadow
 	TweenMax.fromTo($(this), 0.7, {boxShadow: "0px 0px 0px 0px rgba(255,255,51,0.3)"}, {boxShadow: "0px 0px 20px 10px rgba(255,255,51,0.7)",
 	    repeat: -1, yoyo: true, ease: Linear.easeNone});
-},
-function() {// back to normal
-	TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
-});
+	},
+	function() {// back to normal
+		TweenMax.to($(this), 0.15, {scale: 1, boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"});
+});// End right hover
+
+// Click and move function
+function click() {
+	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true, onComplete: hideOpeningPage});
+	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
+	TweenMax.to(logo, 1.5, {x: -725, y: -290, rotation: 360, scale:0.25});// Moves logo
+	TweenMax.to([perfectButton, buttonText], 1, {autoAlpha: 0});// Removes button and text
+};
+
+//hide opening page div onComplete
+function hideOpeningPage() {
+	$("#openingPage").hide();// hides opening page
+	console.log(hideOpeningPage);
+}
 
 // Click on left image - both images move off page
-leftImage.click(function(){
-	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
-	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -650, y: -200, scale:0.35});// Moves logo
-});
+leftImage.click(click);
 
 // Click on right image - both images move off page
-rightImage.click(function(){
-	TweenMax.to(leftImage, 1, {x:-1200, ease:Linear.easeNone, rotation: 360, force3D:true});
-	TweenMax.to(rightImage, 1, {x:1200, ease:Linear.easeNone, rotation: -360, force3D:true});
-	TweenMax.to(logo, 1.5, {x: -650, y: -200, scale:0.35});// Moves logo
+rightImage.click(click);
+
+// Click on button animation
+button.click(click);
+
+$(document).on("mouseenter", ".restaurant", function(){
+	TweenMax.to(restaurant, 1.5, {rotation: 100});
 });
 
+// Restaurant
+/*restaurant.hover(function(){
+	TweenMax.to(restaurant, 1.5, {rotation: 100});
+});*/
+
+// Movie choice animation
+/*$(".movieChoice1").click(function(){
+	TweenMax.to($(".movieChoice"), 1, {rotation: 90});
+});*/
+
+
+/*movieChoice.click(function(){
+	TweenMax.to($(this), 1, {scale: 1.5});
+});*/
+
+/*
+{textShadow:"2px 2px 15px white", color: "red"}
+movieChoice.click(function() {
+	TweenMax.from(restaurantDiv, 2, {//slide from offpage to onpage});
+});
+*/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,7 @@ var database = firebase.database
 
 // Gracenote API and OMDB API
 $(window).on("load", function() {
+
 	var queryURL = "http://data.tmsapi.com/v1.1/movies/showings" + "?startDate=2017-06-13&zip=44131" + "&api_key=zephc9snecc3dpg2eh66m4ng";
 
 	$.ajax({
@@ -43,15 +44,41 @@ $(window).on("load", function() {
 });
 
 // Event Brite API
+$(document).ready(function(){
+  $('#zipCode').click(function(){
+     var zipCode = $('#enteredZipCode').val();
+		 //var enteredName = document.getElementById('enteredFormName').value.replace(' ', '+');
+     if (zipCode.length != 5) {
+       $('#movieEventHolder').html("Invalid zip code");
+       console.log("Wrong");
+       console.log(zipCode);
+     }
+     else {
+       $('#movieEventHolder').val("Displaying events around " + zipCode);
 
-var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=WJ5ZSOV6TV56IC44E7EJ&location.address=Cleveland";
+       //Ajax Request
+      var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=WJ5ZSOV6TV56IC44E7EJ&location.address=" + zipCode;
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
+      $.ajax({
+       url: queryURL,
+       method: "GET"
 
-  }).done(function (event) {
-    //full object
-    console.log(event);
+     }).done(function (event) {
+       //full object
+       //console.log(event);
+       //console.log(event.events[0].logo.url);
+       //console.log (event.location.address);
+       var content =
+       "<br><div id='#movieEventHolder'>" + event.events[10].name.html + "</div><br><br>" +
+       "<div id='#movieEventHolder'>" + event.events[20].name.html + "</div><br><br>" +
+       "<div id='#movieEventHolder'>" + event.events[30].name.html + "</div><br><br>" +
+       "<div id='#movieEventHolder'>" + event.events[40].name.html + "</div><br><br>"
+       ;
+       $("#movieEventHolder").html(content);
+     });
 
-  });
+}
+//closing if/else statement
+
+	    });
+    });
